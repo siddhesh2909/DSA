db28d086bf41b8a5724d83fe85907abaa0731759
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-20 10:49:47 +0530

    Order basis Binary Search

---
+++ Binary Search/src/orderAgnosticBinarySearch.java
@@ -0,0 +1,50 @@
+public class orderAgnosticBinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 9;
+
+        int result= findout(arr,target);
+        if(result!=-1){
+            System.out.println("The target number " + target + " is present in the array at " + result +"th Index");
+        }
+        else {
+            System.out.println("The target number " + target + " is not present in the array");
+        }
+
+    }
+    static int findout(int[] arr , int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (arr[start] < arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == target) {
+                    return mid;
+                }
+                if (arr[mid] > target) {
+                    end = mid - 1;
+                }
+                if (arr[mid] < target) {
+                    start = mid + 1;
+                }
+            }
+        }
+            else {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (arr[mid] == target) {
+                    return mid;
+                }
+                if (arr[mid] > target) {
+                    start = mid + 1;
+                }
+                if (arr[mid] < target) {
+                    end = mid - 1;
+                }
+            }
+        }
+                return -1;
+
+        }
+    }
