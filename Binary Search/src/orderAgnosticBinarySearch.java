b59616d2dc897b24dae407cab4ffbb1747289c36
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-20 10:51:17 +0530

    For reversed array (binary Search)

--- Binary Search/src/orderAgnosticBinarySearch.java
+++ Binary Search/src/orderAgnosticBinarySearch.java
@@ -1,7 +1,8 @@
 public class orderAgnosticBinarySearch {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr= {10,9,8,7,6,5,4,3,2,1};
         int target = 9;
 
         int result= findout(arr,target);
