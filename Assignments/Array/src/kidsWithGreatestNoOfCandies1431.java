f91712ffd5059a7c2db7772fba42ac652218c08c
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-21 10:35:23 +0530

    leetcode 1431

--- Assignments/Array/src/kidsWithGreatestNoOfCandies1431.java
+++ Assignments/Array/src/kidsWithGreatestNoOfCandies1431.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class kidsWithGreatestNoOfCandies {
+public class kidsWithGreatestNoOfCandies1431 {
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies=3;
+        int greatestCandies= Arrays.stream(candies).max().getAsInt();
+        boolean[] arr = new boolean[candies.length];
+        for(int i = 0; i < candies.length; i++){
+                arr[i]= ((candies[i]+extraCandies)>= greatestCandies);
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
 }
