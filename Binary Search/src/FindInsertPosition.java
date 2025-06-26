564f5f10d26fe6b35a70c043eec7035821a03846
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-26 07:43:26 +0530

    find insert position

---
+++ Binary Search/src/FindInsertPosition.java
@@ -0,0 +1,23 @@
+public class FindInsertPosition {
+    public static void main(String[] args) {
+        int[] nums={1};
+        int target=1;
+        int start=0;
+        int end=nums.length-1;
+
+       for(int i=start;i<=end;i++){
+           if(nums[i] >=target){
+               System.out.println(i);
+               return;
+           }
+
+        }
+        if(nums[end]<=target)
+        {
+            System.out.println(end);
+            return;
+        }
+        System.out.println(-1);
+
+    }
+}
