1d1de4127117b1ee256110485b4183075bafbd6f
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-27 11:44:34 +0530

    Incomplete problem

---
+++ Assignments/Array/src/NextPermutation.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NextPermutation {
+    public static void main(String[] args) {
+        int[] nums = {3,2,1};
+        Arrays.sort(nums);
+        List<Integer> ans=new ArrayList<>();
+
+    }
+}
