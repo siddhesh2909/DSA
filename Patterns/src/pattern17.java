46984010628a62774de92afdef27e800ee90e2ac
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-28 09:23:05 +0530

    pattern 17

---
+++ Patterns/src/pattern17.java
@@ -0,0 +1,24 @@
+public class pattern17 {
+    public static void main(String[] args) {
+        int n=4;
+        for(int row=0; row< 2*n ; row++)
+        {
+            int c = row >= n ? 2 * n - row : row;
+
+            for(int spaces = 1; spaces <=n -c; spaces++)
+            {
+                System.out.print(" ");
+            }
+            for(int col = c; col>=1; col--)
+            {
+                System.out.print(col);
+            }
+            for (int col = 2; col <=c; col++)
+            {
+                System.out.print(col);
+            }
+
+            System.out.println();
+        }
+    }
+}
