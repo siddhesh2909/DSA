a017993f9a6395b0c5d26b35fc0e1854794017da
siddhesh2909 <siddhesh2909@gmail.com>
2025-06-28 09:50:44 +0530

    Trying to solve

--- Assignments/Array/src/NextPermutation.java
+++ Assignments/Array/src/NextPermutation.java
@@ -4,9 +4,22 @@ import java.util.List;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] nums = {3,2,1};
-        Arrays.sort(nums);
-        List<Integer> ans=new ArrayList<>();
+        int[] nums = {1,2,3};
+        int i=nums[0];
+        int j=nums[1];
+        int k=nums[2];
+
+        if (i>j && j>k) {
+            int temp= i;
+            i=k;
+            k=temp;
+            System.out.println(i +","+j+","+k);
+        } else if ((i<j && j>k) ||(i>j && j<k || (i<j && j<k)) ) {
+            int temp= j;
+            j=k;
+            k=temp;
+            System.out.println(i +","+j+","+k);
+        }
 
     }
 }
--- Assignments/Array/src/leetcode2099.java
+++ Assignments/Array/src/leetcode2099.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public class leetcode2099 {
+    public static void main(String[] args) {
+        int[] nums ={2,1,3,3};
+        int k=2;
+        for(int i: nums)
+        {
 
-public class leetcode2090 {
+        }
+    }
 }
