38c3516d0f3250320be84f2c2f092d72ebbabf6e
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-01 15:49:17 +0530

    Reverse Words in String

---
+++ String Questions/src/ReversewordsinString.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class ReversewordsinString {
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for(int i=words.length-1;i>=0;i--)
+        {
+            result.append(words[i]);
+            if(i!=0) {
+                result.append(" ");
+            }
+        }
+        System.out.println(result.toString());
+
+    }
+}
