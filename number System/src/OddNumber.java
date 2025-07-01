ffe3d00d5061aa56ae33460973ed7a7ef4657cc2
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-01 08:03:27 +0530

    Number System Even odd

---
+++ number System/src/OddNumber.java
@@ -0,0 +1,9 @@
+public class OddNumber {
+    public static void main(String[] args) {
+        int n=66;
+        System.out.println(isOdd(n));
+    }
+    public static boolean isOdd(int n){
+        return (n & 1) == 1;
+    }
+}
