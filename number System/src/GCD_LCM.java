09d662bd69af6445d7f10c06113760fd712a3e60
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-03 08:24:54 +0530

    GCD

---
+++ number System/src/GCD_LCM.java
@@ -0,0 +1,13 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println(GCD(12,8));
+    }
+    static int GCD(int a,int b){
+        if(a==0)
+        {
+            return b;
+        }
+        return GCD(b%a,a);
+    }
+
+}
