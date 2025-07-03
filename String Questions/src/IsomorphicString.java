1b5e34a088a567acfa69db0cf89f5586e7e15581
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-03 11:11:06 +0530

    Isomorphic String'

--- String Questions/src/IsomorphicString.java
+++ String Questions/src/IsomorphicString.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class IsomorphicString {
+    public static void main(String[] args) {
+        String s ="foo";
+        String t="bar";
+        int[] indexS = new int[200];
+        int[] indexT = new int[200];
+
+        for(int i=0;i<s.length();i++){
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]){
+                System.out.println(false);
+                return;
+            }
+            indexS[s.charAt(i)] = i+1;
+            indexT[s.charAt(i)] = i+1;
+        }
+        System.out.println(true);
+
+    }
 }
