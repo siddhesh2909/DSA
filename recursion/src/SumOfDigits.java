b853e98a5df8f3c7e64f088a8217bfeee2879d0d
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-03 09:49:33 +0530

    Sum of digits

---
+++ recursion/src/SumOfDigits.java
@@ -0,0 +1,11 @@
+public class SumOfDigits {
+    public static void main(String[] args) {
+        System.out.println(sum(1234));
+    }
+    public static int sum(int n){
+        if(n==0) {
+            return 0;
+        }
+        return n%10+sum(n/10);
+    }
+}
