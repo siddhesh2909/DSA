444a86666022a862a9983df16181464f5012b5ab
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-03 08:45:41 +0530

    1to N & N to 1

--- recursion/src/SimpleFun.java
+++ recursion/src/SimpleFun.java
@@ -1,7 +1,8 @@
 public class SimpleFun {
     public static void main(String[] args) {
 //        fun(5);
-        funRev(5);
+//        funRev(5);
+        funBoth(5);
     }
     public static void fun(int n){
         if(n ==0)
@@ -17,6 +18,15 @@ public class SimpleFun {
             return;
         }
         funRev(n-1);
+        System.out.println(n);
+    }
+    public static void funBoth(int n){
+        if(n ==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        funBoth(n-1);
         System.out.println(n);
     }
 }
