d01e56ae92860ecfc4f725d77386a8a79c13afca
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-03 08:30:45 +0530

    LCM

--- number System/src/GCD_LCM.java
+++ number System/src/GCD_LCM.java
@@ -1,6 +1,7 @@
 public class GCD_LCM {
     public static void main(String[] args) {
         System.out.println(GCD(12,8));
+        System.out.println(lcm(2,7));
     }
     static int GCD(int a,int b){
         if(a==0)
@@ -9,5 +10,7 @@ public class GCD_LCM {
         }
         return GCD(b%a,a);
     }
-
+    static int lcm(int a,int b){
+        return (a*b)/GCD(a,b);
+    }
 }
