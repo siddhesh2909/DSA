479c258a798899e01484f42f905e85c312d9b161
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-04 10:18:59 +0530

    Roman To integer

---
+++ String Questions/src/RomanToInteger.java
@@ -0,0 +1,31 @@
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String s = "MCMXCIV";
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int num = value(s.charAt(i));
+
+            if (i < s.length() - 1 && num < value(s.charAt(i + 1))) {
+                res -= num;
+            } else {
+                res += num;
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    public static int value(char ch) {
+        switch (ch) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
