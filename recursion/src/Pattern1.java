2e650be0aef870a96dd7e550b0a86a8a8dd7e15b
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-04 09:23:11 +0530

    Simple Triangle

--- recursion/src/Pattern1.java
+++ recursion/src/Pattern1.java
@@ -1,8 +1,9 @@
 public class Pattern1 {
     public static void main(String[] args) {
-        triangle(4,0);
+        triangle1(4,0);
+        triangle2(5,0);
     }
-    public static void triangle(int r, int c){
+    public static void triangle1(int r, int c){
         if(r==0)
         {
             return;
@@ -10,11 +11,27 @@ public class Pattern1 {
         if(c<r)
         {
             System.out.print("*");
-            triangle(r, c+1);
+            triangle1(r, c+1);
         }
         else{
             System.out.println();
-            triangle(r-1, 0);
+            triangle1(r-1, 0);
         }
+    }
+    public static void triangle2(int r, int c){
+        if(r==0)
+        {
+            return;
+        }
+        if(c<r)
+        {
+            triangle2(r, c+1);
+            System.out.print("*");
+        }
+        else {
+            triangle2(r-1, 0);
+            System.out.println();
+        }
+
     }
 }
