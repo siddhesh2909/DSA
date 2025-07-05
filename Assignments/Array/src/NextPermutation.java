a5c7089ed604f8d7046bd20e146900541eb1cb71
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-05 09:50:22 +0530

    next Permutation

--- Assignments/Array/src/NextPermutation.java
+++ Assignments/Array/src/NextPermutation.java
@@ -1,25 +1,39 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class NextPermutation {
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
-        int i=nums[0];
-        int j=nums[1];
-        int k=nums[2];
+        int[] nums = {1, 2, 3};
+        int n = nums.length;
+        int i = n - 2;
 
-        if (i>j && j>k) {
-            int temp= i;
-            i=k;
-            k=temp;
-            System.out.println(i +","+j+","+k);
-        } else if ((i<j && j>k) ||(i>j && j<k || (i<j && j<k)) ) {
-            int temp= j;
-            j=k;
-            k=temp;
-            System.out.println(i +","+j+","+k);
+        // Step 1: Find the pivot
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
         }
 
+        // Step 2: If pivot found, swap with rightmost successor
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(nums, i + 1, n - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i++, j--);
+        }
     }
 }
