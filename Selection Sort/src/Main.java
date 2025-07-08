14bc2a095cf02a445f5ce9e09f22a965fd74e6bb
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-08 17:31:18 +0530

    selection Sort

--- Selection Sort/src/Main.java
+++ Selection Sort/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            int[] arr = {5,3,1,7,2};
+            int[] arr = {5,3,1,2};
             for(int i=0; i<arr.length; i++){
                 int last = arr.length-1-i;
                 int max= findMaxIndex(arr ,0 , last);
