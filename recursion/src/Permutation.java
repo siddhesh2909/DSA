e314c20b4ed4e8b2d360763b04b8f0e1c9c313c2
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-08 08:24:52 +0530

    Permuation list

--- recursion/src/Permutation.java
+++ recursion/src/Permutation.java
@@ -19,4 +19,20 @@ public class Permutation {
         }
     }
 
+    public static ArrayList<String> permutaton(String a , String b){
+        if(b.isEmpty())
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(a);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        char ch = b.charAt(0);
+        for (int i = 0; i <= a.length(); i++) {
+            String f = a.substring(0, i);
+            String s = a.substring(i);
+            ans.addAll(permutaton(f + ch + s, b.substring(1)));
+        }
+        return ans;
+    }
 }
