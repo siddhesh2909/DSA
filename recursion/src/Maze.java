39ffb13ffa43a2e448dfd10aefdd87d09410e13e
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-09 08:08:57 +0530

    Maze Count

---
+++ recursion/src/Maze.java
@@ -0,0 +1,19 @@
+public class Maze {
+    public static void main(String[] args) {
+        System.out.println(mazeCount(3,3));
+        mazePath("",3,3);
+    }
+    public static int mazeCount(int r, int c)
+    {
+        if(r==1 || c==1)
+        {
+            return 1;
+        }
+
+        int left = mazeCount(r-1,c);
+        int right = mazeCount(r,c-1);
+
+        return left+right;
+    }
+
+}
