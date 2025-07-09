b8ffd262eb3d53bbbd0a39bf182f340bed664b36
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-09 07:44:15 +0530

    Dice as List

--- recursion/src/Dice.java
+++ recursion/src/Dice.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dice {
     public static void main(String[] args) {
     dice("",4);
+        System.out.println(dice1("",4));
     }
     public static void dice(String p , int target)
     {
@@ -14,6 +18,24 @@ public class Dice {
         {
             dice(p+i,target-i);
         }
+    }
+
+    public static List<String> dice1(String p , int target)
+    {
+        if(target == 0)
+        {
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        List<String> list = new ArrayList<>();
+
+        for(int i=1; i<=6 && i<=target ; i++)
+        {
+            list.addAll(dice1(p+i,target-i));
+        }
+        return list;
     }
 }
 
