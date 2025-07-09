82cb3c69ae93a758c24758fec6524d0375691bd8
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-09 08:11:26 +0530

    Maze path as ArrayList

--- recursion/src/Maze.java
+++ recursion/src/Maze.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         System.out.println(mazeCount(3,3));
         mazePath("",3,3);
+        System.out.println(mazePathList("",3,3));
     }
     public static int mazeCount(int r, int c)
     {
@@ -32,5 +35,24 @@ public class Maze {
             mazePath(p+'R',r,c-1);
         }
 
+    }
+    public static ArrayList<String> mazePathList(String p, int r, int c)
+    {
+        if(r==1 && c==1)
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1)
+        {
+            list.addAll(mazePathList(p+'D',r-1,c));
+        }
+        if(c>1)
+        {
+            list.addAll(mazePathList(p+'R',r,c-1));
+        }
+        return list;
     }
 }
