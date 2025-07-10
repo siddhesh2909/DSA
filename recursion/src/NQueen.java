df1e84060ffa73a1f5d01d6246277ac8147511b2
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-10 08:10:25 +0530

    n Queen Problem

---
+++ recursion/src/NQueen.java
@@ -0,0 +1,63 @@
+public class NQueen {
+    public static void main(String[] args) {
+    boolean[][] board = new boolean[4][4];
+    System.out.println( nQueen(board,0));
+    }
+    public static int nQueen(boolean[][] board , int row) {
+        if(row == board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count = 0;
+        for(int col = 0; col < board.length; col++){
+                if(isSafe(board, row, col)){
+                    board[row][col] = true;
+                    count += nQueen(board, row + 1);
+                    board[row][col] = false;
+                }
+
+        }
+        return count;
+    }
+    public static boolean isSafe(boolean[][] board, int row, int col) {
+        //vertical
+        for(int i = 0; i < row; i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+        // for left digonal
+        int maxLeft= Math.min(row, col);
+        for(int i = 1; i <= maxLeft; i++){
+            if(board[row-i][col-i]){
+                return false;
+            }
+        }
+        //for right digonal
+        int maxRight= Math.min(row, board.length-col-1);
+        for(int i = 1; i <= maxRight; i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+           for(boolean col : row) {
+               if(col)
+               {
+                   System.out.print('Q');
+               }
+               else {
+                   System.out.print('X');
+               }
+           }
+           System.out.println();
+        }
+    }
+
+
+}
