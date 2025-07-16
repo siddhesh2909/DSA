e479d879f2053d2997701dae64fc4f523d8608bc
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-16 10:03:50 +0530

    Circular Queue

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,15 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        customQueue queue = new customQueue();
-        queue.push(10);
-        queue.push(20);
-        queue.push(30);
-        queue.push(40);
-        queue.push(50);
+//        customQueue queue = new customQueue();
+//        queue.push(10);
+//        queue.push(20);
+//        queue.push(30);
+//        queue.push(40);
+//        queue.push(50);
+//
+//        queue.display();
+//        queue.remove();
+//        queue.display();
 
-        queue.display();
-        queue.remove();
-        queue.display();
+        circularQueue circularQueue = new circularQueue(5);
+        circularQueue.push(10);
+        circularQueue.push(20);
+        circularQueue.push(30);
+        circularQueue.push(40);
+        circularQueue.push(50);
+
+        circularQueue.remove();
+        circularQueue.push(60);
+
+        circularQueue.display();
+
+
 
         }
 
---
+++ Queue/src/circularQueue.java
@@ -0,0 +1,73 @@
+public class circularQueue {
+    protected int[] data;
+
+    private static final int DEFAULT_SIZE = 10;
+
+    protected int front=0;
+    protected int end = 0;
+    protected int size = 0;
+
+    public circularQueue()
+    {
+        this(DEFAULT_SIZE);
+    }
+
+    public circularQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == data.length ;
+    }
+
+    public boolean push(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        data[end++] = value;
+        end %= data.length;
+        size++;
+        return true;
+    }
+
+    public int remove()
+    {
+        if (isEmpty()) {
+            return -1;
+        }
+        int removed= data[front++];
+        front %= data.length;
+        size --;
+
+        return removed;
+    }
+    public int front()
+    {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[front];
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty");
+            return;
+        }
+
+        int i = front;
+        do {
+            System.out.print(data[i] + "<--");
+            i = (i + 1) % data.length;
+        } while (i != end);
+
+        System.out.println("END");
+    }
+
+
+}
