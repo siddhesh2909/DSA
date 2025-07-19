df0ef4b2e7ec2902a6a6610a73a2ff35bf89b284
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-19 10:41:08 +0530

    Modified @D sorted Search

--- Assignments/Array/src/SearchIn2DMetrix2.java
+++ Assignments/Array/src/SearchIn2DMetrix2.java
@@ -3,29 +3,30 @@ import java.util.Arrays;
 public class SearchIn2DMetrix2 {
     public static void main(String[] args) {
         int[][] matrix =
-                {{1, 4, 7, 11, 15},
-                {2, 5, 8, 12, 19},
-                {3, 6, 9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}};
+                {
+                        {-1,3}
+                };
+//                {{1, 4, 7, 11, 15},
+//                {2, 5, 8, 12, 19},
+//                {3, 6, 9, 16, 22},
+//                {10, 13, 14, 17, 24},
+//                {18, 21, 23, 26, 30}};
 
 
-        System.out.println(Arrays.toString(searchmat(matrix,5)));
-
+        System.out.println(Arrays.toString(searchmat(matrix,3)));
 
     }
     public static int[] searchmat(int[][] matrix, int target) {
         int row=0;
-        int col= matrix.length-1;
+        int col = matrix[0].length - 1;
         while(row<matrix.length && col>=0){
             if(matrix[row][col]==target){
                 return new int[]{row,col};
-            }
-            if(matrix[row][col]<target){
-                row++;
+            } else if(matrix[row][col] > target){
+                col--;
             }
             else{
-                col--;
+                row++;
             }
 
         }
