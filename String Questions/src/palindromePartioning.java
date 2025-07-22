a13c3609ffac02b5c729dbcdce5c56966bc17d61
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-22 11:02:21 +0530

    Palindrome Partitioning

---
+++ String Questions/src/palindromePartioning.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class palindromePartioning {
+
+    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (isPalindrome(s, start, end - 1)) {
+                path.add(s.substring(start, end));
+                backtrack(s, end, path, result);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
+}
