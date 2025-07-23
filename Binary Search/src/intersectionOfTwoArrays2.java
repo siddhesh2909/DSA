198f46929abde0f120ef38efc0d86787ec7a5e5a
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-23 09:28:59 +0530

    intersection of two Arrays

--- Binary Search/src/intersectionOfTwoArrays2.java
+++ Binary Search/src/intersectionOfTwoArrays2.java
@@ -6,21 +6,27 @@ public class intersectionOfTwoArrays2 {
     public static void main(String[] args) {
         int[] nums1 = {4,9,5};
         int[] nums2 = {9,4,9,8,4};
-        List<Integer> list1 = new ArrayList<>();
-        for (int num : nums1) {
-            list1.add(num);
-        }
-        List<Integer> list2 = new ArrayList<>();
-        for (int num : nums2) {
-            if(list1.contains(num)){
-                list2.add(num);
+        int l1 = nums1.length;
+        int l2 = nums2.length;
+        int i = 0, j = 0, k = 0;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        while( i < l1 && j < l2)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else if(nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                nums1[k++] = nums1[i++];
+                j++;
             }
         }
-        int[] arr = new int[list2.size()];
-        int i = 0;
-        for(int num:list2){
-            arr[i++]=num;
-        }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(Arrays.copyOfRange(nums1, 0, k)));
     }
 }
