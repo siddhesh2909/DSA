e49149f4d33b6cac2125a440d81fef74b6c53e1e
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-23 10:34:14 +0530

    TO Lower Case

--- String Questions/src/ToLowerCase.java
+++ String Questions/src/ToLowerCase.java
@@ -1,7 +1,17 @@
 public class ToLowerCase {
     public static void main(String[] args) {
         String s = "Hello";
-        s = s.toLowerCase();
-        System.out.println(s);
+//        s = s.toLowerCase();
+//        System.out.println(s);
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch >= 'A' && ch <= 'Z'){
+                sb.append((char)(ch+32));
+            }
+            else {
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
