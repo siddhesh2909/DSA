c39c7c56fd9d0bfb0d26a0f432bea60fc000a2a9
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-24 10:08:36 +0530

    Delete Node

---
+++ LinkedList/src/Questions/DeleteNodeExample.java
@@ -0,0 +1,31 @@
+package Questions;
+
+public class DeleteNodeExample {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        // Creating the list: 4 -> 5 -> 1 -> 9
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        DeleteNodeExample obj = new DeleteNodeExample();
+
+        obj.deleteNode(head.next);
+
+        printList(head);
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
