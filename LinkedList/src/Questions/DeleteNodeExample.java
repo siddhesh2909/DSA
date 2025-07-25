d5f8124f3a630aaf5c2a5f49a48da45d71f33c58
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-25 11:31:14 +0530

    Remove Elements

--- LinkedList/src/Questions/DeleteNodeExample.java
+++ LinkedList/src/Questions/DeleteNodeExample.java
@@ -28,4 +28,20 @@ public class DeleteNodeExample {
         }
         System.out.println();
     }
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return dummy.next;
+
+    }
 }
