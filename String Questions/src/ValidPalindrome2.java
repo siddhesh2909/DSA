b7d3e0fb513490b21356bcb43469f828f5308060
siddhesh2909 <siddhesh2909@gmail.com>
2025-07-25 10:52:47 +0530

    Valid Palindrome 2

---
+++ String Questions/src/ValidPalindrome2.java
@@ -0,0 +1,30 @@
+public class ValidPalindrome2 {
+    public static void main(String[] args) {
+        String s= "deeee";
+
+    }
+    public static boolean validPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
