c52b57707707dd6f70a381deab9d896ce3bb896e
siddhesh2909 <siddhesh2909@gmail.com>
2026-05-08 10:13:40 +0530

    Concatination of Array

---
+++ Arrays/src/ConcatenationofArray1929.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ConcatenationofArray1929 {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i < n ; i++)
+        {
+            nums[i]=sc.nextInt();
+        }
+
+        int len = 2*n;
+        int[] ans = new int[len];
+
+        for(int i=0; i < len  ; i++)
+        {3
+            ans[i]=nums[i % n];
+        }
+
+        System.out.printf(Arrays.toString(ans));
+    }
+}
