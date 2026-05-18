e2dd0e128e1d3a34d896030d7eec7ca93f85a4bf
siddhesh2909 <siddhesh2909@gmail.com>
2026-05-18 16:53:31 +0530

    Rotate Array

---
+++ Arrays/src/RotateArray.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main( String[] args)
+    {
+        int[] arr = {1,2,3,4,5,6,7};
+        int k=3;
+
+        int n= arr.length;
+        int m = k %n;
+
+        reverse(arr , 0, n-1);
+        reverse(arr,0,k-1);
+        reverse(arr, k, n-1);
+
+        System.out.println(Arrays.toString(arr));
+
+
+    }
+
+    public static void reverse(int[] arr , int s , int e){
+        while(s<e)
+        {
+            int temp = arr[s];
+            arr[s]= arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
+        }
+    }
+}
