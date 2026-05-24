77e1d4ea2afd1ca6893f84adfbaefa3c0638f4f0
siddhesh2909 <siddhesh2909@gmail.com>
2026-05-24 11:49:11 +0530

    Optimized Majority Element

--- Arrays/src/MajorityElement.java
+++ Arrays/src/MajorityElement.java
@@ -3,22 +3,39 @@ import java.util.HashMap;
 public class MajorityElement {
     public static void main(String[] args)
     {
-        int[] nums = {7, 0, 0, 1, 7, 7, 2, 7, 7};
-        HashMap<Integer , Integer> map = new HashMap<>();
+    int[] nums = {7, 0, 0, 1, 7, 7, 2, 7, 7};
+//        HashMap<Integer , Integer> map = new HashMap<>();
+//
+//        for(int num : nums)
+//        {
+//            map.put(num, map.getOrDefault(num, 0) + 1);
+//
+//            if(map.get(num) > nums.length/2)
+//            {
+//                System.out.println(num);
+//                return;
+//            }
+//        }
+//
+//        System.out.println(-1);
 
-        for(int num : nums)
+        int candidate=0;
+        int count=0;
+
+        for(int num :nums)
         {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (count == 0) {
+                candidate = num;
+            }
 
-            if(map.get(num) > nums.length/2)
-            {
-                System.out.println(num);
-                return;
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
             }
         }
 
-        System.out.println(-1);
-
+        System.out.println(candidate);
 
     }
 }
