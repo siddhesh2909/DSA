56402fc741849feda4b925cf79b3ccc2cb718406
siddhesh2909 <siddhesh2909@gmail.com>
2026-05-24 11:45:07 +0530

    Majority Element ||

---
+++ Arrays/src/MajorityElement2.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MajorityElement2 {
+    public static void main(String[] args)
+    {
+      int[] nums = {3,2,3};
+//
+//        List<Integer> list = new ArrayList<>();
+//        HashMap<Integer,Integer> map = new HashMap<>();
+//
+//        for(int num : nums)
+//        {
+//            map.put(num, map.getOrDefault(num,0)+1);
+//
+//            if(map.get(num) > nums.length/3)
+//            {
+//                if(!list.contains(num))
+//                {
+//                    list.add(num);
+//                }
+//
+//            }
+//        }
+//
+//        System.out.println(list);
+
+        int count1 =0;
+        int count2=0;
+        int candidate1=0;
+        int candidate2=0;
+
+        for(int num : nums)
+        {
+            if(candidate1==num)
+            {
+                count1++;
+            } else if (candidate2 == num) {
+            count2++;
+            } else if (count1==0) {
+                candidate1=num;
+                count1 = 1;
+
+            } else if (count2==0) {
+            candidate2=num;
+            count2++;
+            } else{
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        count1=0;
+        count2=0;
+
+        for(int num : nums)
+        {
+            if(num == candidate1)
+            {
+                count1++;
+            }
+            if(num== candidate2)
+            {
+                count2++;
+            }
+
+
+        }
+        int n = nums.length;
+
+        if(count1 > n/3)
+        {
+            list.add(candidate1);
+        }
+        if(count2> n/3)
+        {
+            list.add(candidate2);
+        }
+
+        System.out.println(list);
+    }
+}
