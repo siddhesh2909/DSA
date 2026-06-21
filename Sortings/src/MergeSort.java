f1135736af2407883dd11cba8e0322740ad0b57c
siddhesh2909 <siddhesh2909@gmail.com>
2026-06-21 11:06:10 +0530

    Sorting (Merge Sort)

---
+++ Sortings/src/MergeSort.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args)
+    {
+        int nums[] ={5,2,4,6,1};
+        int n = nums.length;
+        System.out.println(Arrays.toString(mergeSortt(nums)));
+
+    }
+
+    static int[] mergeSortt(int[] nums)
+    {
+            if(nums.length == 1){
+                return nums;
+            }
+
+        int mid = nums.length /2;
+        int[] left = mergeSortt(Arrays.copyOfRange(nums, 0 , mid));
+        int[] right = mergeSortt(Arrays.copyOfRange(nums,mid,nums.length));
+
+        return merge(left,right);
+    }
+    static int[] merge( int[] left , int [] right)
+    {
+
+        int mix[] = new int[left.length+ right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+
+        while(i < left.length && j < right.length)
+        {
+            if(left[i] < right[j])
+            {
+                mix[k++]= left[i++];
+            }else {
+                mix[k++]= right[j++];
+            }
+        }
+        while(i< left.length)
+        {
+            mix[k++]= left[i++];
+        }
+        while(j< right.length)
+        {
+            mix[k++]= right[j++];
+        }
+        return mix;
+    }
+}
