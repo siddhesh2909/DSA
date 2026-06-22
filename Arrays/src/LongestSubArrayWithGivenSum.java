3e07d64bbe2e6f9a5c6b3141e6a6b40ec0a64f16
siddhesh2909 <siddhesh2909@gmail.com>
2026-06-22 11:34:19 +0530

    Longesh Sub Array with given Sum

--- Arrays/src/LongestSubArrayWithGivenSum.java
+++ Arrays/src/LongestSubArrayWithGivenSum.java
@@ -9,21 +9,21 @@ public class LongestSubArrayWithGivenSum {
         int sum=0;
         int maxLen = 0;
 
-        for(int i=0; i< arr.length ; i++)
+        for(int i=0; i<arr.length; i++)
         {
             sum+=arr[i];
 
-            if(sum == k)
+            if(sum ==k)
             {
-                maxLen= i+1;
+                maxLen=i+1;
             }
 
-            int rem = sum - k;
+            int rem = sum -k;
 
             if(map.containsKey(rem))
             {
-                int len = i -map.get(rem);
-                maxLen= Math.max(len,maxLen);
+                int len = i - map.get(rem);
+                maxLen=Math.max(maxLen,len);
             }
 
             if(!map.containsKey(sum))
