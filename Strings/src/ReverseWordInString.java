28e5d208515a61e4ccd6a84bd0e7c5b472d3ebcd
siddhesh2909 <siddhesh2909@gmail.com>
2026-06-22 10:08:23 +0530

    Reverse Words in Strings

--- Strings/src/ReverseWordInString.java
+++ Strings/src/ReverseWordInString.java
@@ -5,15 +5,15 @@ public class ReverseWordInString {
     {
         String s ="the sky is blue";
 
-        String[] arr = s.trim().split("\\s+");
+        String arr[] = s.trim().split("\\s+");
 
         StringBuilder sb = new StringBuilder();
 
-        for(int i = arr.length - 1; i >= 0; i--) {
-
+        for(int i= arr.length-1; i>=0 ; i--)
+        {
             sb.append(arr[i]);
-
-            if(i != 0) {
+            if(i!=0)
+            {
                 sb.append(" ");
             }
         }
