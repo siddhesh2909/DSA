c66f2cab833250c9cf14a5a1aed55b592f2cf176
siddhesh2909 <siddhesh2909@gmail.com>
2026-06-24 14:51:55 +0530

    Three Sum Problem On Arrays

---
+++ Arrays/src/THreeeSum.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class THreeeSum {
+    public static void main(String[] args)
+    {
+        int[] nums = {-1,0,1,2,-1,-4};
+        int n= nums.length;
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0 ; i < n ; i++)
+        {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i+1;
+            int right = n-1;
+
+            while (left < right)
+            {
+                int sum = nums[left] + nums[right]+ nums[i];
+
+                if(sum < 0)
+                {
+                    left++;
+                }else if(sum > 0)
+                {
+                    right--;
+                }else {
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left-1])
+                    {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right+1])
+                    {
+                        right--;
+                    }
+                }
+            }
+        }
+        System.out.println(list);
+    }
+}
