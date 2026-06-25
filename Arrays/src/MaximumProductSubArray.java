aabb7fe6ca7d78527c92e01d39329a74ff8be0c5
siddhesh2909 <siddhesh2909@gmail.com>
2026-06-25 13:30:37 +0530

    Maximum Product Subarray

---
+++ Arrays/src/MaximumProductSubArray.java
@@ -0,0 +1,19 @@
+public class MaximumProductSubArray {
+    public static void main(String[] args)
+    {
+        int[] nums= {-1,-2,-3};
+        int prefix =1;
+        int suffix=1;
+        int ans=Integer.MIN_VALUE;
+
+        for(int i=0; i< nums.length;i++)
+        {
+            if(prefix==0) prefix=1;
+            if(suffix==0) suffix=1;
+            prefix*=nums[i];
+            suffix*=nums[nums.length-1-i];
+            ans=Math.max(ans,Math.max(prefix,suffix));
+        }
+        System.out.println(ans);
+    }
+}
